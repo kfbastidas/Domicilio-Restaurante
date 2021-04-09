2b120158fe9f0f4a1db9a3ca4245bf5d30e35432
kevith bastidas <kevithchi@unicauca.edu.co>
2021-04-09 01:56:16 -0500

    Restaurante online

--- Documentos/AppRestauranteOnline/restaurante-server/src/test/java/co/mycompany/restaurante/server/domain/RestauranteServiceTest.java
+++ Documentos/AppRestauranteOnline/restaurante-server/src/test/java/co/mycompany/restaurante/server/domain/RestauranteServiceTest.java
@@ -4,11 +4,9 @@ import co.mycompany.restaurante.commons.domain.DiaSemana;
 import co.mycompany.restaurante.commons.domain.Plato;
 import co.mycompany.restaurante.server.domain.services.RestauranteService;
 import co.mycompany.restaurante.commons.domain.Restaurante;
-import co.mycompany.restaurante.commons.domain.TipoComponente;
 import co.mycompany.restaurante.server.access.Factory;
 import co.mycompany.restaurante.server.access.IRestauranteRepository;
 import com.google.gson.Gson;
-import java.util.Date;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
@@ -30,67 +28,52 @@ public class RestauranteServiceTest {
         System.out.println(resp);
         assertEquals("Plato update correctamente",resp);
     }
-//    @Test
-//    public void testgetProtocol() {  
-//        System.out.println("darProtocol");      
-//        IRestauranteRepository repo = Factory.getInstance().getRepository();
-//        RestauranteService instance = new RestauranteService(repo);
-//        
-//        Componente c = new Componente(11, "lentejas", TipoComponente.ENTRADA);
-//        
-//        assertEquals(3,3);
-//    }
-//    @Test
-//    public void testgetMenuDia() {  
-//        System.out.println("darMenuDia del id restaurante: 1 y dia Lunes");      
-//        IRestauranteRepository repo = Factory.getInstance().getRepository();
-//        RestauranteService instance = new RestauranteService(repo);
-//        
-//        ArrayList<Componente> menu = instance.getMenuDia(1,DiaSemana.LUNES); 
-//        
-//        Gson gson = new Gson();
-//        String strObject = gson.toJson(menu);
-//        System.out.println(strObject);
-////        System.out.println(menu);
-//        assertEquals(3, menu.size());
-//    }   
-//    @Test
-//    public void testgetRestaurantes() {  
-//        System.out.println("darRestaurantes");      
-//        IRestauranteRepository repo = Factory.getInstance().getRepository();
-//        RestauranteService instance = new RestauranteService(repo);
-//        
-//        ArrayList<Restaurante> restaurante = instance.getRestaurantes(); 
-//        Gson gson = new Gson();
-//        String strObject = gson.toJson(restaurante);
-//        System.out.println(strObject);
-////        System.out.println(restaurante);
-//        assertEquals(1, restaurante.size());
-//        assertEquals(1, restaurante.get(0).getMenuPlatos().size());
-//    }      
-//    @Test
-//    public void testgetComponentes() {  
-//        System.out.println("darComponentes");      
-//        IRestauranteRepository repo = Factory.getInstance().getRepository();
-//        RestauranteService instance = new RestauranteService(repo);
-//        
-//        ArrayList<Componente> componentes = instance.getComponentes(); 
-//         Gson gson = new Gson();
-//        String strObject = gson.toJson(componentes);
-//        System.out.println(strObject);
-////        System.out.println(componentes);
-//        assertEquals(13, componentes.size());
-//    }      
-//    @Test
-//    public void testgetAdministrador() {  
-//        System.out.println("darAdministradro");      
-//        IRestauranteRepository repo = Factory.getInstance().getRepository();
-//        RestauranteService instance = new RestauranteService(repo);
-//        
-//        String clave = instance.getAdministrador("admin");
-//        System.out.println("clave: "+clave);
-//        assertEquals(clave, "123456");
-//    }  
+    @Test
+    public void testgetMenuDia() {  
+        System.out.println("darMenuDia del id restaurante: 1 y dia Lunes");      
+        IRestauranteRepository repo = Factory.getInstance().getRepository();
+        RestauranteService instance = new RestauranteService(repo);
+        
+        ArrayList<Componente> menu = instance.getMenuComponentes(1, DiaSemana.LUNES); 
+        
+        Gson gson = new Gson();
+        String strObject = gson.toJson(menu);
+        System.out.println(strObject);
+        assertEquals(15, menu.size());
+    }   
+    @Test
+    public void testgetRestaurantes() {  
+        System.out.println("darRestaurantes");      
+        IRestauranteRepository repo = Factory.getInstance().getRepository();
+        RestauranteService instance = new RestauranteService(repo);
+        
+        ArrayList<Restaurante> restaurante = instance.getRestaurantes(); 
+        assertEquals(4, restaurante.size());
+    }      
+    @Test
+    public void testgetComponentes() {  
+        System.out.println("darComponentes");      
+        IRestauranteRepository repo = Factory.getInstance().getRepository();
+        RestauranteService instance = new RestauranteService(repo);
+        
+        ArrayList<Componente> componentes = instance.getComponentes(); 
+         Gson gson = new Gson();
+        String strObject = gson.toJson(componentes);
+        System.out.println(strObject);
+        assertEquals(19, componentes.size());
+    }      
+    @Test
+    public void testgetAdministrador() {  
+        System.out.println("darAdministradro");      
+        IRestauranteRepository repo = Factory.getInstance().getRepository();
+        RestauranteService instance = new RestauranteService(repo);
+        
+        String clave = instance.getAdministrador("admin");
+        System.out.println("clave: "+clave);
+        assertEquals(clave, "123456");
+    }  
+    /*
+    //Pasa la prueba solo cuando esta la base de datos vacia
 //    @Test
 //    public void testaddComponente() {  
 //        System.out.println("addComponente");      
@@ -124,4 +107,5 @@ public class RestauranteServiceTest {
 //        System.out.println(result);
 //        assertEquals(result, "Se añadido Correctamente el menu semanal");
 //    }  
+    */
 }
